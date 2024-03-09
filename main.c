219e43b17889eab14c647f88bb05a9924f91196d
iarimbola <rabeiarmbola@gmail.com>
2024-03-09 12:47:59 +0100

    main 2.0

--- main.c
+++ main.c
@@ -31,7 +31,10 @@ int main(int argc, char ** argv){
     }
 
     if(strcmp(argv[1], "-E") == 0){
-        //int construit arbre ici
+        /*
+        int construit arbre ici
+        serialise(Arbre);
+        */
         return 1;
     }
 
