56c6340def3c21524a22d1b6c23cd8aa1e878a06
AnriOfAstora94 <154369215+AnriOfAstora94@users.noreply.github.com>
2024-03-09 13:00:00 +0100

    Euh oui oui

--- main.c
+++ main.c
@@ -31,10 +31,10 @@ int main(int argc, char ** argv){
     }
 
     if(strcmp(argv[1], "-E") == 0){
-        /*
-        int construit arbre ici
-        serialise(Arbre);
-        */
+
+        Arbre a = NULL;
+        construit_arbre(&a);
+       //serialise(a); Pas compris ici
         return 1;
     }
 
